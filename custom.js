8718f9290b8916e3cfcfd20db3736d2cd94267ba
Pawan <pawansingh9@gmail.com>
2015-10-02 16:49:15 -0500

    Before Gabriel's Autocomplete addition

--- custom.js
+++ custom.js
@@ -1,84 +1,103 @@
-var myCallbackFn = function(data)
+var context = new hd.ContextBuilder()
+	.variables({keyword: '', list: []})
+	.constraint('keyword, list')
+	.method('keyword -> list', function(keyword){				
+		return fetchCityList(keyword); }) 
+	.context();
+
+	var pm = new hd.PropertyModel();
+	console.log("PropertyModel created");
+	
+	pm.addComponent(context);
+	window.addEventListener('load', function(){
+	hd.performDeclaredBindings(context);
+	});	
+
+function AutoComplete(el)
 {
-	console.log("Testing JSON Object");
-	console.log(JSON.stringify(data));
+	this.el = el;
+}
 
+AutoComplete.prototype.onNext = function(cities)
+{
+	$(this.el).autocomplete({source: cities});
+}
 
+AutoComplete.prototype.onError = function()
+{
+	console.log("onError");
+}
+
+AutoComplete.prototype.onCompleted = function()
+{
+	console.log("onCompleted");
 }
 
 
-$( document ).ready(function() {
-	$("#locationTextHd").attr('autocomplete', 'off');
-	$("#locationTextHd").keyup(function(e){
-		
-		
-		keywordHd = $("#locationTextHd").val();
-		var p = new hd.Promise();
-		var q = p.then(
-			
-				function fetch(keywordHd){
-				var x = new hd.Promise();
-				$.ajax({
-				      url: 'http://autocomplete.wunderground.com/aq?&format=json&cb=myCallbackFn&query=New'+ keywordHd,
-				      error: function() {
-				         //$('#info').html('<p>An error has occurred</p>');
-				      },
-				      dataType: 'json',
-				      success: function(data) {
-						
-						var cities = [];  
-				    	for(i=0;i<data.length;i++)
-				    	{ cities.push(data[i].cityName); }
-				    	x.resolve( cities );
-				    	
-				      },
-				      type: 'GET'
-				   })
-				   return x;
-			}
-		
-		);
-		
-		
-		q.then(
-				function populate(citiesHd)
-				{	
-					$("#locationTextHd").autocomplete({
-			    	     source: citiesHd
-			    	   });
-					$("#locationTextHd").attr('autocomplete', 'on');
-					
-				}
-		);	
-		
-			
+function fetchCityList(keyword) {
+
+	var p = new hd.Promise();
+	var cities = [];
 	
-	p.resolve(keywordHd);	
-	});
-	
-	
+        $.ajax({
+            type: 'GET',
+            url: 'http://autocomplete.wunderground.com/aq?cb=call=?',
+            data: {
+                "query": keyword
+            },
+            async: false,
+            dataType: 'jsonp',
+            crossDomain: true,
+            success: function(response) {
+               $.each(response.RESULTS, function(i) {
+                    out = (response.RESULTS[i].name);
+                    cities.push(out);
+                });
+               
+               console.log(cities);
+               p.resolve(cities);
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+    
+    return p;
+    
 
-	
-	
-	
-});	
+}
+
+$(document).ready(function() {
+
+    $("#locationTextWithoutHd").keydown(function() {
+    	//$("#locationTextWithoutHd").autocomplete('destroy');
+    	var keyword = $("#locationTextWithoutHd").val();
+        var url = 'http://autocomplete.wunderground.com/aq?format=json&cb=myCallbackFn&query=' + keyword;
+        $.ajax({
+            type: 'GET',
+            url: url,
+            async: true,
+            dataType: 'jsonp',
+            error: function(e) {
+                console.log(e.message);
+            }
+        });
+    });
 
 
 
+});
 
-function fun() 
-{
-  	var url = 'http://autocomplete.wunderground.com/aq?query=New&format=json&cb=myCallbackFn';
+var myCallbackFn = function(data) {
+    var cities = [];
+    for (i = 0; i < data.RESULTS.length; i++) {
+        if (data.RESULTS[i].type == 'city') {
+            cities.push(data.RESULTS[i].name);
+        }
+    }
 
-	$.ajax({
-	   type: 'GET',
-	    url: url,
-	    async: true,
-	    dataType: 'jsonp',
-	    error: function(e) {
-	       console.log(e.message);
-	    }
-	});
-
-
+    $("#locationTextWithoutHd").autocomplete({
+        source: cities
+    }).autocomplete("widget").addClass("fixed-height");
 }
